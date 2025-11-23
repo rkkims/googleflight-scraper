efe5df9518f5bdbfaac0fd64b5516bebc773dac5
Richard Kim <richard.k.kim.work@gmail.com>
2025-11-23 02:03:47 +0000

    bug fixed

--- src/serializer/flight_serializer.py
+++ src/serializer/flight_serializer.py
@@ -3,7 +3,7 @@ import flights_pb2 as pb
 from base64 import urlsafe_b64decode, urlsafe_b64encode
 from datetime import datetime
 import json
-
+import sys
 
 class FlightSerializer:
     @staticmethod
